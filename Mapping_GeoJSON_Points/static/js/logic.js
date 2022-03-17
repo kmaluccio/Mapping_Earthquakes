333c6dc9c5dc0197bdfb953d6315a7700336c183
kmaluccio <kmaluccio@tamu.edu>
2022-03-16 19:08:19 -0500

    updated code for multiple layers and options for viewing the map

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,17 +1,39 @@
 // Create the map object with a center and zoom level
-let map = L.map('mapid').setView([30, 30], 2);
+// let map = L.map('mapid').setView([30, 30], 2);
 
-// Create the tile layer that will be the background of our map
+// Create multiple tile layers that will be the background of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
 // Then add our tile layer to the map
-streets.addTo(map);
+// streets.addTo(map);
+
+// Create map object with center and zoom level and default layer
+let map = L.map('mapid', {
+    center: [40.7, -94.5],
+    zoom: 4,
+    layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map
+L.control.layers(baseMaps).addTo(map);
+
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://github.com/kmaluccio/Mapping_Earthquakes/blob/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/kmaluccio/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grab geojson data and add to our map
 d3.json(airportData).then(function(data) {
@@ -20,6 +42,6 @@ d3.json(airportData).then(function(data) {
     L.geoJSON(data, {
         onEachFeature: function(feature, layer) {
             layer.bindPopup("<h2>" + "Airport code: " + feature.properties.faa + "</h2>" + "__________________________________________" + "<h3>" + "Airport name: " + feature.properties.name + "</h3>");
-        }
-    }).addTo(map);
+        }}
+    ).addTo(map);
 });
